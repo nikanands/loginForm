9f151a7e5346efd5b03423c218d0747cb76d5671
Nikhil Anand <nikhil@Nikhils-MacBook-Air.local>
2019-05-24 12:11:52 +0530

    Updates

---
+++ precache-manifest.41bc5e225b663f7b85fe28e46674478a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "400d33fec811c3908c582c0ff668a9f1",
+    "url": "/login/index.html"
+  },
+  {
+    "revision": "a0904104426d3c342507",
+    "url": "/login/static/css/main.b76dde84.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/login/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "a0904104426d3c342507",
+    "url": "/login/static/js/main.2eaa17f4.chunk.js"
+  },
+  {
+    "revision": "d036c0eadd0ee639ddc1",
+    "url": "/login/static/js/runtime~main.299f3dfc.js"
+  }
+]);
