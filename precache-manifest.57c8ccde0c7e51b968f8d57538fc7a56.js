c2aab2aa0087d84784202e0f19226cf084ce5452
Nikhil Anand <nikhil@Nikhils-MacBook-Air.local>
2019-05-24 16:56:12 +0530

    Updates

---
+++ precache-manifest.57c8ccde0c7e51b968f8d57538fc7a56.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2621f403124d1a7c3b54051e3136514",
+    "url": "/loginForm/index.html"
+  },
+  {
+    "revision": "a0904104426d3c342507",
+    "url": "/loginForm/static/css/main.b76dde84.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/loginForm/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "a0904104426d3c342507",
+    "url": "/loginForm/static/js/main.2eaa17f4.chunk.js"
+  },
+  {
+    "revision": "babb71e356b75525dd56",
+    "url": "/loginForm/static/js/runtime~main.a568ae1c.js"
+  }
+]);
